bb4223cfb0f2ea80b11e1cd25623675ebc23bcd9
matisse babonneau <matisse.babonneau@orange.com>
2022-11-30 22:19:44 +0100

    f

--- example/App.go
+++ example/App.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"syscall/js"
 
 	o "github.com/Matbabs/Gooroo"
@@ -10,9 +8,7 @@ import (
 
 type Person struct {
 	name string
-	age  int
-	has  bool
-	flt  float64
+	age  string
 }
 
 func App() o.DomComponent {
@@ -20,15 +16,11 @@ func App() o.DomComponent {
 	o.Css("")
 
 	name, _ := o.UseState("")
-	age, _ := o.UseState(42)
-	_bool, _ := o.UseState(false)
-	_flt, _ := o.UseState(0.3)
+	age, _ := o.UseState("42")
 	p, setP := o.UseState(Person{})
 
 	handleSubmit := func(_ js.Value) {
-		age_i, _ := strconv.Atoi((*age).(string))
-		setP(Person{(*name).(string), age_i, (*_bool).(bool), (*_flt).(float64)})
-		fmt.Println((*p).(Person))
+		setP(Person{(*name).(string), (*age).(string)})
 	}
 
 	return o.Div(o.Style("margin: auto; padding: 100px; width: 800px"),
@@ -43,6 +35,7 @@ func App() o.DomComponent {
 			o.Button("Click", o.OnClick(handleSubmit), o.Title("")),
 			o.If(*age != nil,
 				o.P((*p).(Person).name),
+				o.P((*p).(Person).age),
 			),
 		),
 	)
